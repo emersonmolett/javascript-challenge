06638e9400fa700222e0c313ef2da6a93f4ad5f3
Emerson Molett <emerson.molett@gmail.com>
2021-04-16 17:25:41 -0500

    updated code to loop through ufosightings

--- 14-Intro-To-JavaScript/UFO-level-1/static/js/app.js
+++ 14-Intro-To-JavaScript/UFO-level-1/static/js/app.js
@@ -2,3 +2,12 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+function createTable(sightings) {
+    tableBody.html("");
+
+    // go through objects
+    sightings.forItems(function(ufoSighting) {
+
+        console.log(ufoSighting);
+    })
+}
