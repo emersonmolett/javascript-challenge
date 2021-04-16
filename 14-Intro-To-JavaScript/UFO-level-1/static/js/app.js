99de3ee84074680191bce8c10d4ba8ea970c5738
Emerson Molett <emerson.molett@gmail.com>
2021-04-16 17:41:39 -0500

    set variables to stoe emements for table population

--- 14-Intro-To-JavaScript/UFO-level-1/static/js/app.js
+++ 14-Intro-To-JavaScript/UFO-level-1/static/js/app.js
@@ -6,8 +6,21 @@ function createTable(sightings) {
     tableBody.html("");
 
     // go through objects
-    sightings.forItems(function(ufoSighting) {
+    sightings.forItems(function (ufoSighting) {
 
         console.log(ufoSighting);
-    })
-}
+
+        //set variable to store elemnts for population of table rows
+        var tableRow = tableBody.append("tr");
+
+        Object.defineProperties(ufoSighting).forEach(function ([objectKey, ObjectValue]) {
+
+            console.log(objectKey, ObjectValue);
+
+            var infoCell = tableRow.append("td");
+
+            // append value to table
+            infoCell.text(ObjectValue);
+        });
+    });
+};
