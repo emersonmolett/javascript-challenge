6a02d90a224081800f89fc1aba1795a35e1a7b18
Emerson Molett <emerson.molett@gmail.com>
2021-04-17 19:10:14 -0500

    removed an extra folder that was copy/paste into ufo-level-2, pushing updates

---
+++ 14-Intro-To-JavaScript/UFO-level-2/static/js/app.js
@@ -0,0 +1,100 @@
+// from data.js
+var tableData = data;
+
+// YOUR CODE HERE!
+function createTable(sightings) {
+    // use d3 to display table function on webpage
+    var tableBody = d3.select("tbody");
+    tableBody.html("");
+
+    // go through objects
+    sightings.forEach(function (ufoSighting) {
+
+        // console.log(ufoSighting);
+
+        //set variable to store elemnts for population of table rows
+        var tableRow = tableBody.append("tr");
+
+        Object.entries(ufoSighting).forEach(function ([objectKey, ObjectValue]) {
+
+            // console.log(objectKey, ObjectValue);
+
+            var infoCell = tableRow.append("td");
+
+            // append value to table
+            infoCell.text(ObjectValue);
+        });
+    });
+};
+
+// work on filtering for table by the entered date
+function filterTable() {
+
+    // stop reload
+    // d3.event.presentDefault();
+    // var userInputs = [];
+    // create variable for storing input date 
+    // var storeValue = d3.selectAll(".form-control")
+    var dateValue = d3.select("#datetime").property("value")
+    var cityValue = d3.select("#city").property("value")
+    var stateValue = d3.select("#state").property("value")
+    var countryValue = d3.select("#country").property("value")
+    var shapeValue = d3.select("#shape").property("value")
+    // console.log(storeValue)
+    // storeValue.forEach(userInput =>{
+    //    // userInputs.push(userInput)
+    //    console.log(userInput[0])
+    // })
+    // console.log(userInputs)
+    //set variable to set a copy for data filtering
+    var filterData = tableData;
+
+    // do conditional statement to reset page when clicked button 
+    if (dateValue != "") {
+        filterData = filterData.filter(function (sightingRow) {
+            // need a conditional statement by using a boolean, to display table row
+            if (dateValue === sightingRow.datetime) { return true; }
+        })
+    }
+
+    if (cityValue != "") {
+        filterData = filterData.filter(function (sightingRow) {
+            // need a conditional statement by using a boolean, to display table row
+            if (cityValue === sightingRow.city) { return true; }
+        })
+    }
+
+    if (stateValue != "") {
+        filterData = filterData.filter(function (sightingRow) {
+            // need a conditional statement by using a boolean, to display table row
+            if (stateValue === sightingRow.state) { return true; }
+        })
+    }
+    if (countryValue != "") {
+        filterData = filterData.filter(function (sightingRow) {
+            // need a conditional statement by using a boolean, to display table row
+            if (countryValue === sightingRow.country) { return true; }
+        })
+    }
+    if (shapeValue != "") {
+        filterData = filterData.filter(function (sightingRow) {
+            // need a conditional statement by using a boolean, to display table row
+            if (shapeValue === sightingRow.shape) { return true; }
+        })
+    }
+
+    createTable(filterData);
+
+
+}
+// call function
+createTable(tableData);
+
+// allow filter table button to be referenced by variable & be input
+var filterButton = d3.select("#filter-btn");
+var dataEnter = d3.select("#datetime");
+
+// allow for either enter or filter table to be used
+filterButton.on("click", filterTable);
+dataEnter.on("submit", filterTable);
+
